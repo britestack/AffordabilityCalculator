2bee9d9f5be4df64a31dd59bea10be83b6b07b31
Jacky Xia <jacky.w.xia@gmail.com>
2021-02-06 18:27:49 -0800

    Implement all table generators

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,30 @@
-DROP SCHEMA IF EXISTS affordabilityCalculator CASCADE;
-CREATE SCHEMA IF NOT EXISTS affordabilityCalculator AUTHORIZATION sdc_user;
+DROP DATABASE IF EXISTS sdc;
+CREATE DATABASE IF NOT EXISTS sdc;
+
+/c sdc
 
 CREATE TABLE IF NOT EXISTS listings (
   listing_id SERIAL PRIMARY KEY,
-  price integer,
-  rating decimal,
+  price integer NOT NULL,
+  rating decimal NOT NULL,
 );
 CREATE TABLE IF NOT EXISTS users (
-  usr_id SERIAL PRIMARY KEY,
-  username VARCHAR(25),
-  email VARCHAR(50),
-  user_password VARCHAR(25),
-  user_ip integer
+  users_id SERIAL PRIMARY KEY,
+  username VARCHAR(25) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  user_password VARCHAR(25) NOT NULL,
+  user_ip VARCHAR(45) NOT NULL
 )
 CREATE TABLE IF NOT EXISTS mortgages (
   mortgage_id SERIAL PRIMARY KEY,
-  mortgage_name VARCHAR(25),
-  terms text[],
-  fees integer,
-  rate decimal,
-  apr decimal
+  mortgage_name VARCHAR(25) NOT NULL,
+  terms text[] NOT NULL,
+  fees integer NOT NULL,
+  rate decimal NOT NULL,
+  apr decimal NOT NULL,
+  users_id integer NOT NULL REFERENCES users(users_id),
+  listing_id integer NOT NULL REFERENCES listings(listing_id)
 )
-COPY listings (listing_id, price, rating) FROM './db/listings.csv';
-COPY users (usr_id, username, email, user_password, user_ip) FROM './db/users.csv';
-COPY mortgages (mortgage_id, mortgage_name, terms, fees, rate, apr) FROM './db/mortgages.csv';
+\COPY listings (listing_id, price, rating) FROM './db/listings.csv' WITH CSV HEADER DELIMITER ',';
+\COPY users (usr_id, username, email, user_password, user_ip) FROM './db/users.csv' WITH CSV HEADER DELIMITER ',';
+\COPY mortgages (mortgage_id, mortgage_name, terms, fees, rate, apr) FROM './db/mortgages.csv' WITH CSV HEADER DELIMITER ',';
